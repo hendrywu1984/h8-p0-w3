493ee25febebcb4f97068bf62261cc9a47392638
hendry <hendry_wu1984@yahoo.com>
2019-10-29 18:16:45 +0700

    Finish

--- Exercises 6.js
+++ Exercises 6.js
@@ -1,31 +1,19 @@
 function angkaPalindrome(num) {
     // you can only write your code here!
-    var strNum = '';
-    var isOk = false;
+    var str1 = '';
+    var str2;
 
-    while (isOk === false){
-        num += 1;
-        strNum = num.toString();
+    do {
+        str2 = '';
+        num++;
+        str1 = num.toString();
 
-        if (hasilPalindrome(strNum) === true) isOk = true;
-        else isOk = false;
-    }
-    return strNum;
-  }
-
-  function hasilPalindrome(strNum) {
-    var ok = 0;
-    var setengah = Math.floor(strNum.length / 2);
-    
-    if (setengah > 0) {
-        for (var i = 0; i < Math.floor(strNum.length / 2); i++){
-            if (strNum[i] === strNum[strNum.length-(i+1)]) ok++;
+        for(var i = 0; i < str1.length; i++) {
+            str2+=str1[str1.length-(i+1)];
         }
-        if (ok === Math.floor(strNum.length / 2)) return true;
-        else return false;
-    } else return true;
-
-  }
+    } while (str1 != str2);
+    return parseInt(str2);
+}
   
   // TEST CASES
   console.log(angkaPalindrome(8)); // 9
