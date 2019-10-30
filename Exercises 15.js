e42f6b6e31a61c9c3cc3cde8ccbb29aef19abbb8
hendry <hendry_wu1984@yahoo.com>
2019-10-30 14:37:00 +0700

    Tugas Phase 0 Week 3 Exercises 15

---
+++ Exercises 15.js
@@ -0,0 +1,30 @@
+function groupAnimals(animals) {
+  // you can only write your code here!
+  animals.sort();
+  
+  var hasil = [];
+  var temp = [];
+  var awal;
+
+  temp.push(animals[0]);
+  awal = animals[0][0];
+
+  for (var i=1; i<animals.length; i++) {
+      if (awal === animals[i][0]) {
+          temp.push(animals[i]);
+      } else {
+          awal = animals[i][0];
+          hasil.push(temp);
+          temp = [];
+          temp.push(animals[i]);
+      }
+  }
+  hasil.push(temp);
+  return hasil;
+}
+
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
